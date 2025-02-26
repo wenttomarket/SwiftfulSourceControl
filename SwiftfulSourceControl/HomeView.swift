254694555bb42cc6403c1ac109dec82d48e68938
Darrell Stone <darrellstone3@gmail.com>
2025-02-26 15:14:29 -0500

    home view finished

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Screen 2")
+        VStack{
+            Text("Screen 2")
+            
+            Text("Screen 2")
+            Text("Screen 2")
+        }
+        
     }
 }
 
