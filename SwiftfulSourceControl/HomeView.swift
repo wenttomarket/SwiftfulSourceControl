42f1f14811f6c8e700cc640e95677ad4cfe25880
Darrell Stone <darrellstone3@gmail.com>
2025-02-26 15:13:32 -0500

    add home view

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Darrell Stone on 2/26/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
