af5b4b81b54509776e84f6e878c95810c782fb94
Darrell Stone <darrellstone3@gmail.com>
2025-02-26 11:36:49 -0500

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Darrell Stone on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulSourceControlTests/SwiftfulSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlTests.swift
+//  SwiftfulSourceControlTests
+//
+//  Created by Darrell Stone on 2/26/25.
+//
+
+import Testing
+@testable import SwiftfulSourceControl
+
+struct SwiftfulSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
