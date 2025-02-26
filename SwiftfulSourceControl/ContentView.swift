4b4d8cb213800f8538f17a2f67fb7e198032e843
Darrell Stone <darrellstone3@gmail.com>
2025-02-26 15:06:51 -0500

    another commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,6 +15,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Swiftful Thinking!")
+            
+            Button("Click me!") {
+                
+            }
         }
         .padding()
     }
