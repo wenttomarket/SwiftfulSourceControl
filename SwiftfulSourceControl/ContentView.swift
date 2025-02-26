876e94eed4fa60063d692758fcf2a6e2179bfc04
Darrell Stone <darrellstone3@gmail.com>
2025-02-26 15:25:07 -0500

    [Clean] re-order commit documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Darrell Stone on 2/26/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the bug
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] description of the changes
+ 
+ RELEASE
+ [Release] Description of release
+ */
+
+
+
 import SwiftUI
 
 struct ContentView: View {
